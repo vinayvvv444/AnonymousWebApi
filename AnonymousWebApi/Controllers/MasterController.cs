de9083d48d8645a4bcf4e4455b26283426af6974
Vinay Valsan <vinayvvv444@gmail.com>
2021-01-12 15:29:41 +0400

    master changes

--- AnonymousWebApi/Controllers/MasterController.cs
+++ AnonymousWebApi/Controllers/MasterController.cs
@@ -37,7 +37,7 @@ namespace AnonymousWebApi.Controllers
         private DistrictRepository _districtRepository;
 
         // changes done from development[changes from development branch] branch
-        // changes done from [ changes from master branch] development branch
+        // changes done from [ changes from master branch] [master changes] development branch
 
         ///Master controller
         public MasterController(IMapper mapper,
